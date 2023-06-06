64ae5eb888d9610ac64e871478a671b5c236458f
Brock Kennedy <brock97k@gmail.com>
2023-06-06 07:11:35 -0300

    Two Crystal Ball

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,18 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const jmpAmount = Math.floor(Math.sqrt(breaks.length))
+    let i = jmpAmount
+    for (; i < breaks.length; i += jmpAmount){
+        if (breaks[i]) {
+            break;
+        }
+    }
 
+    i -= jmpAmount;
+    const iStart = i;
+    for (; i < iStart + jmpAmount && i < breaks.length; i++){
+        if (breaks[i]){
+            return i;
+        }
+    }
+    return -1
 }
